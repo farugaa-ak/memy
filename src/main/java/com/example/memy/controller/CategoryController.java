12147c917e04a6e198dc654bbc1a30452d519439
Jakub Kaczynski <jakubkaczynski93@gmail.com>
2017-08-06 16:29:02 +0200

    working in progress

--- src/main/java/com/example/memy/controller/CategoryController.java
+++ src/main/java/com/example/memy/controller/CategoryController.java
@@ -10,16 +10,14 @@ import com.example.memy.Model.Gif;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class CategoryController {
 
     private CategoryDaoImpl categoryDaoImpl = new CategoryDaoImpl();
+    private GifDaoImpl gifDao= new GifDaoImpl();
 
     @GetMapping("/categories")
     public String article(ModelMap modelMap){
@@ -29,25 +27,15 @@ public class CategoryController {
 
     @GetMapping("/category/{id}")
     public String display(@PathVariable Long id, ModelMap modelMap){
-        GifDaoImpl gifDao= new GifDaoImpl();
         modelMap.addAttribute("category", categoryDaoImpl.getCategory(id));
         modelMap.addAttribute("gifs", gifDao.gifsInCategory(id));
-
-
-@Controller
-public class CategoryController {
-
-    @GetMapping("/categories")
-    public String article(ModelMap modelMap){
-        CategoryDaoImpl categoryDao= new CategoryDaoImpl();
-        modelMap.addAttribute("categories", categoryDao.findAll());
-        return "categories";
-    }
-    @GetMapping("/category/{id}")
-    public String display(@PathVariable Long id, @ModelAttribute Category category, ModelMap modelMap){
-        CategoryDaoImpl categoryDaoImpl= new CategoryDaoImpl();
-        modelMap.addAttribute("gifs", categoryDaoImpl.gifsInCategory(category.getId()).getGifs());
-
         return ("category");
+    }
+
+    @GetMapping("/category/search")
+    public String result(@RequestParam String q, ModelMap modelMap){
+        modelMap.addAttribute("category", categoryDaoImpl.findByName(q));
+        modelMap.addAttribute("gifs", gifDao.gifsInCategory(q));
+        return "category";
     }
 }
--- src/main/java/com/example/memy/controller/GifController.java
+++ src/main/java/com/example/memy/controller/GifController.java
@@ -16,32 +16,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GifController {
-    GifDaoImpl gifDaoImpl=new GifDaoImpl();
+
+    private GifDaoImpl gifDaoImpl=new GifDaoImpl();
+
     @GetMapping("/")
     public String home(ModelMap modelMap){
-
         modelMap.addAttribute("gifs", gifDaoImpl.allGifs());
         return "home";
     }
+
     @GetMapping("/gif/{name}")
     public String display(@PathVariable String name, @ModelAttribute Gif gif, ModelMap modelMap){
-
         modelMap.addAttribute("gif",gifDaoImpl.findByName(name).get(0));
-
-
-        modelMap.addAttribute("allgifs",gifDaoImpl.allGifs());
-
         return ("gif-details");
     }
+
     @GetMapping("/favorites")
     public String showFavorites(ModelMap modelMap){
         modelMap.addAttribute("gifs",gifDaoImpl.findFavourites());
-    return ("favorites");
+        return ("favorites");
     }
+
     @GetMapping("/gifs/search")
     public String result(@RequestParam String q, ModelMap modelMap){
         modelMap.addAttribute("gifs", gifDaoImpl.findByName(q));
         return "home";
     }
-
 }

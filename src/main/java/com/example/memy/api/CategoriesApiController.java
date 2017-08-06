62e0dc0d0ef5bcbc9b4acd98a766b560c4a5ac27
Jakub Kaczynski <jakubkaczynski93@gmail.com>
2017-08-06 18:35:33 +0200

    working in progress

--- src/main/java/com/example/memy/api/CategoriesApiController.java
+++ src/main/java/com/example/memy/api/CategoriesApiController.java
@@ -1,7 +1,21 @@
 package com.example.memy.api;
 
+import com.example.memy.ArticleDao.CategoryDaoImpl;
+import com.example.memy.Model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by Qbala on 2017-08-06.
  */
+@RestController
 public class CategoriesApiController {
+
+    @GetMapping("/api/categories")
+    public List<Category> listCategories(){
+        CategoryDaoImpl categoryDao = new CategoryDaoImpl();
+        return categoryDao.findAll();
+    }
 }
